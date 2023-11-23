1de2e0807a4bcb7179bb3a36f4a3e392cf88eab4
Wilber <wilber.vargas@teclab.com.co>
2023-11-23 16:07:22 -0500

    feat: insert all TB_PEDIDOS_BARCODE_CAJA`  when `MANIFIESTO_URBANO`

--- models/rotulosPlModel.js
+++ models/rotulosPlModel.js
@@ -1,8 +1,9 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 class RotulosPlModel {
   static async findByPk(codigoDeBarrasReq) {
-    const query = 'SELECT CODIGO_PK, TB_PEDIDOS_BARCODE_CAJA, MANIFIESTO_URBANO, PLACA_DE_REPARTO, ESTADO FROM TB_PEDIDOS_DIGITALIZADO WHERE TB_PEDIDOS_BARCODE_CAJA = ?';
+    const query =
+      "SELECT CODIGO_PK, TB_PEDIDOS_BARCODE_CAJA, MANIFIESTO_URBANO, PLACA_DE_REPARTO, ESTADO FROM TB_PEDIDOS_DIGITALIZADO WHERE TB_PEDIDOS_BARCODE_CAJA = ?";
     const values = [codigoDeBarrasReq];
 
     try {
@@ -18,12 +19,52 @@ class RotulosPlModel {
         MANIFIESTO_URBANO,
         PLACA_DE_REPARTO,
         ESTADO,
+        MENSAJE: INICIO,
       } = rotuloPl[0];
 
-      // Ahora puedes realizar un insert utilizando estas variables
-      const insertQuery = 'INSERT INTO TB_VALIDACION_ROTULOS (`TB_PEDIDOS_BARCODE_CAJA`, `MANIFIESTO_URBANO`, `PLACA_DE_REPARTO`, `ESTADO`) VALUES (?, ?, ?, ?)';
-      const insertValues = [TB_PEDIDOS_BARCODE_CAJA, MANIFIESTO_URBANO, PLACA_DE_REPARTO, ESTADO];
-      await db.query(insertQuery, insertValues);
+      // Verificar si ya existe el MANIFIESTO_URBANO en TB_VALIDACION_ROTULOS
+      const checkQuery =
+        "SELECT * FROM TB_VALIDACION_ROTULOS WHERE MANIFIESTO_URBANO = ?";
+      const checkValues = [MANIFIESTO_URBANO];
+      const [existingRecord] = await db.query(checkQuery, checkValues);
+
+      // Si no existe, realizar el insert de todos los registros con el mismo MANIFIESTO_URBANO
+      if (existingRecord.length === 0) {
+        console.log("No existe el manifiesto : " + [MANIFIESTO_URBANO]);
+
+        const insertQuery =
+          "INSERT INTO TB_VALIDACION_ROTULOS (`TB_PEDIDOS_BARCODE_CAJA`, `MANIFIESTO_URBANO`, `PLACA_DE_REPARTO`, `ESTADO`) SELECT `TB_PEDIDOS_BARCODE_CAJA`, `MANIFIESTO_URBANO`, `PLACA_DE_REPARTO`, `ESTADO` FROM TB_PEDIDOS_DIGITALIZADO WHERE MANIFIESTO_URBANO = ?";
+        const insertValues = [MANIFIESTO_URBANO];
+        await db.query(insertQuery, insertValues);
+
+        const updateQuery =
+          "UPDATE `TB_VALIDACION_ROTULOS` SET `LEIDO` = '1' WHERE TB_PEDIDOS_BARCODE_CAJA = ?";
+        const updateValues = [TB_PEDIDOS_BARCODE_CAJA];
+        await db.query(updateQuery, updateValues);
+        /*
+        const upsertQuery =
+          "INSERT INTO TB_VALIDACION_ROTULOS (`TB_PEDIDOS_BARCODE_CAJA`, `MANIFIESTO_URBANO`, `PLACA_DE_REPARTO`, `ESTADO`, `LEIDO`) VALUES (?, ?, ?, ?, 0) ON DUPLICATE KEY UPDATE `LEIDO` = 1";
+        const upsertValues = [
+          TB_PEDIDOS_BARCODE_CAJA,
+          MANIFIESTO_URBANO,
+          PLACA_DE_REPARTO,
+          ESTADO,
+        ];
+        await db.query(upsertQuery, upsertValues);
+        */
+      } else {
+        console.log("Nuevo manifiesto : " + [MANIFIESTO_URBANO]);
+        // Si existe, realizar el update en TB_PEDIDOS_BARCODE_CAJA, LEIDO=1
+        const upsertQuery =
+          "INSERT INTO TB_VALIDACION_ROTULOS (`TB_PEDIDOS_BARCODE_CAJA`, `MANIFIESTO_URBANO`, `PLACA_DE_REPARTO`, `ESTADO`, `LEIDO`) VALUES (?, ?, ?, ?, 1) ON DUPLICATE KEY UPDATE `LEIDO` = 1";
+        const upsertValues = [
+          TB_PEDIDOS_BARCODE_CAJA,
+          MANIFIESTO_URBANO,
+          PLACA_DE_REPARTO,
+          ESTADO,
+        ];
+        await db.query(upsertQuery, upsertValues);
+      }
 
       return rotuloPl[0];
     } catch (error) {
