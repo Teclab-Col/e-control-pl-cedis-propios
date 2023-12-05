a7cca590987e5837f8679d710987f70be064688c
Wilber <wilber.vargas@teclab.com.co>
2023-12-05 10:12:23 -0500

    feat: solucion insertar datos en pool1

--- models/consultarManifiestosModel.js
+++ models/consultarManifiestosModel.js
@@ -33,78 +33,32 @@ class ConsultarManifiestosModel {
 
   static async insertDataIntoPool1(rowDataWithoutPK) {
     try {
-      /*
-          // Obtén las columnas de la tabla de destino
-          const destinationColumns = [
-              'TB_PEDIDOS_BARCODE_CAJA', 'MANIFIESTO_URBANO', 'PLACA_DE_REPARTO', 'ESTADO',
-              'TB_PEDIDOS_MARCA', 'TB_PEDIDOS_CODIGO_ZONA', 'TB_PEDIDOS_CIUDAD', 'TB_PEDIDOS_TIPO_PRODUCTO',
-              'TB_PEDIDOS_CEDULA', 'TB_PEDIDOS_VEREDA', 'TB_PEDIDOS_MUNICIPIO_ENVIEXPRESS',
-              'TIPO_DE_CARGA', 'TIPO_DESTINO', 'VALOR_UNITARIO', 'LEIDO', 'CODE_SESSION'
-          ];
-  
-          // Añade valores predeterminados para todas las columnas
-          const defaultValues = {
-              TB_PEDIDOS_BARCODE_CAJA: 'valor_predeterminado_barcode',
-              MANIFIESTO_URBANO: 'valor_predeterminado_manifiesto_urbano',
-              PLACA_DE_REPARTO: 'valor_predeterminado_palca_de_reparto',
-              ESTADO: 'valor_predeterminado_estado',
-              TB_PEDIDOS_MARCA: 'valor_predeterminado_marca',
-              TB_PEDIDOS_CODIGO_ZONA: 'valor_predeterminado_codigo_zona',
-              TB_PEDIDOS_CIUDAD: 'valor_predeterminado_ciudad',
-              TB_PEDIDOS_TIPO_PRODUCTO: 'valor_predeterminado_tipo_producto',
-              TB_PEDIDOS_CEDULA: 'valor_predeterminado_cedula',
-              TB_PEDIDOS_VEREDA: 'valor_predeterminado_vereda',
-              TB_PEDIDOS_MUNICIPIO_ENVIEXPRESS: 'valor_predeterminado_municipio_enviexpress',
-              TIPO_DE_CARGA: 'valor_predeterminado_carga',
-              TIPO_DESTINO: 'valor_predeterminado_destino',
-              VALOR_UNITARIO: '0',
-              LEIDO: '0',
-              CODE_SESSION: 'NAN'
-          };
-  
-          // Completa los datos de entrada con los valores predeterminados
-          const rowDataWithDefaults = { ...defaultValues, ...rowDataWithoutPK };
-  
-          // Obtén los valores correspondientes a las columnas seleccionadas
-          const values = destinationColumns.map(column => rowDataWithDefaults[column]);
-  
-          // Crea placeholders para la consulta de inserción
-          const placeholders = Array(values.length).fill('?').join(', ');
-  
-          // Construye la consulta de inserción
-          const queryInsert = `INSERT INTO TB_VALIDACION_ROTULOS (${destinationColumns.join(', ')}) VALUES (${placeholders})`;
-  
-          console.log('Query de inserción:', queryInsert);
-          console.log('Valores a insertar:', values);
- */
-      // Ejecuta la consulta de inserción
       const queryInsert = `
-            INSERT INTO TB_VALIDACION_ROTULOS (TB_PEDIDOS_BARCODE_CAJA, MANIFIESTO_URBANO, PLACA_DE_REPARTO, ESTADO, TB_PEDIDOS_MARCA, TB_PEDIDOS_CODIGO_ZONA, TB_PEDIDOS_CIUDAD, TB_PEDIDOS_TIPO_PRODUCTO, TB_PEDIDOS_CEDULA)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        `;
+                INSERT INTO TB_VALIDACION_ROTULOS (TB_PEDIDOS_BARCODE_CAJA, MANIFIESTO_URBANO, PLACA_DE_REPARTO, ESTADO, TB_PEDIDOS_MARCA, TB_PEDIDOS_CODIGO_ZONA, TB_PEDIDOS_CIUDAD, TB_PEDIDOS_TIPO_PRODUCTO, TB_PEDIDOS_CEDULA)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            `;
 
       const valuesInsert = [
-        data.TB_PEDIDOS_BARCODE_CAJA,
-        data.MANIFIESTO_URBANO,
-        data.PLACA_DE_REPARTO,
-        data.ESTADO,
-        data.TB_PEDIDOS_MARCA,
-        data.TB_PEDIDOS_CODIGO_ZONA,
-        data.TB_PEDIDOS_CIUDAD,
-        data.TB_PEDIDOS_TIPO_PRODUCTO,
-        data.TB_PEDIDOS_CEDULA,
+        rowDataWithoutPK.TB_PEDIDOS_BARCODE_CAJA,
+        rowDataWithoutPK.MANIFIESTO_URBANO,
+        rowDataWithoutPK.PLACA_DE_REPARTO,
+        rowDataWithoutPK.ESTADO,
+        rowDataWithoutPK.TB_PEDIDOS_MARCA,
+        rowDataWithoutPK.TB_PEDIDOS_CODIGO_ZONA,
+        rowDataWithoutPK.TB_PEDIDOS_CIUDAD,
+        rowDataWithoutPK.TB_PEDIDOS_TIPO_PRODUCTO,
+        rowDataWithoutPK.TB_PEDIDOS_CEDULA,
       ];
 
       // Utiliza la pool de conexión para realizar la inserción
       await pool1.query(queryInsert, valuesInsert);
 
-      console.log(`Datos insertados en pool1: ${JSON.stringify(data)}`);
-
-      const result = await pool1.query(queryInsert, values);
-
-      console.log("Inserción exitosa. Resultado:", result);
+      console.log(
+        `Datos insertados en pool1: ${JSON.stringify(rowDataWithoutPK)}`
+      );
     } catch (error) {
       console.error("Error en la inserción:", error);
+      throw error;
     }
   }
 }
