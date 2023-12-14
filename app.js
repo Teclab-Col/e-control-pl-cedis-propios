77ea5c511004b58816635a647321b92df7d16378
Wilber <wilber.vargas@teclab.com.co>
2023-12-14 14:17:33 -0500

    feat: server

--- app.js
+++ app.js
@@ -27,18 +27,27 @@ app.use("/api/auth", authRoutes);
 // ! usar este para el servidor remoto
 // Lee los archivos del certificado y la clave privada
 
-/*
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/e-control-pl.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/e-control-pl.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/e-control-pl.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/e-control-pl.com/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/e-control-pl.com/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/e-control-pl.com/chain.pem",
+  "utf8"
+);
 const credentials = { key: privateKey, cert: certificate, ca: ca };
 const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(PORT, IP, () => {
   console.log(`Servidor en ejecución en https://${IP}:${PORT}`);
 });
-*/
 
+/*
 //? Usar este para el servidor local
 app.listen(PORT, () => {
   console.log(`Servidor http escuchando en http://localhost:${PORT}`);
 });
+*/
