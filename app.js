b905433e8d9c3305374b7dc4d421d47650b3615b
Santiago Sierra Cano <santiscano@gmail.com>
2023-12-14 12:39:50 -0500

    fix: se crean las 2 formas de conexion, local y remota

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const rotulosPlRoutes = require('./routes/rotulosPl');
 const consultarManifiestoRoutes = require('./routes/consultarManifiesto');
 const https = require('https');
 const fs = require('fs');
+require('dotenv').config();
 
 const app = express();
 const IP = '0.0.0.0'; // Reemplaza con la dirección IP específica de tu servidor
-const PORT = 3000;
+const PORT = process.env.LOCAL_PORT || 3001;
 
 // Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(bodyParser.json());
@@ -23,14 +24,19 @@ app.use('/api/rotulosPl', rotulosPlRoutes);
 app.use('/api/consultarManifiesto', consultarManifiestoRoutes);
 app.use('/api/auth', authRoutes);
 
+// ! usar este para el servidor remoto
 // Lee los archivos del certificado y la clave privada
-const privateKey = fs.readFileSync('server.key', 'utf8');
-const certificate = fs.readFileSync('server.cert', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-// Crea un servidor HTTPS
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/e-control-pl.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/e-control-pl.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/e-control-pl.com/chain.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate, ca: ca };
 const httpsServer = https.createServer(credentials, app);
-
 httpsServer.listen(PORT, IP, () => {
   console.log(`Servidor en ejecución en https://${IP}:${PORT}`);
 });
+
+// ? Usar este para el servidor local
+// app.listen(app.get("port"), () => {
+//   console.log(`Servidor http escuchando en http://localhost:${PORT}`)
+// })
+
