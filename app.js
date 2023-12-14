690a1843656d3ce1506a8ba5ed41583e4e966314
Wilber <wilber.vargas@teclab.com.co>
2023-12-14 14:50:00 -0500

    feat: protocol

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 const app = express();
 const IP = "0.0.0.0"; // Reemplaza con la dirección IP específica de tu servidor
 const PORT = process.env.LOCAL_PORT || 3001;
-
+const PROTOCOL = process.env.APP_ENV;
 // Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(bodyParser.json());
 
@@ -24,30 +24,29 @@ app.use("/api/rotulosPl", rotulosPlRoutes);
 app.use("/api/consultarManifiesto", consultarManifiestoRoutes);
 app.use("/api/auth", authRoutes);
 
-// ! usar este para el servidor remoto
 // Lee los archivos del certificado y la clave privada
-
-const privateKey = fs.readFileSync(
-  "/etc/letsencrypt/live/e-control-pl.com/privkey.pem",
-  "utf8"
-);
-const certificate = fs.readFileSync(
-  "/etc/letsencrypt/live/e-control-pl.com/cert.pem",
-  "utf8"
-);
-const ca = fs.readFileSync(
-  "/etc/letsencrypt/live/e-control-pl.com/chain.pem",
-  "utf8"
-);
-const credentials = { key: privateKey, cert: certificate, ca: ca };
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(PORT, IP, () => {
-  console.log(`Servidor en ejecución en https://${IP}:${PORT}`);
-});
-
-/*
-//? Usar este para el servidor local
-app.listen(PORT, () => {
-  console.log(`Servidor http escuchando en http://localhost:${PORT}`);
-});
-*/
+if (PROTOCOL == "http") {
+  //? Usar este para el servidor local
+  app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en http://localhost:${PORT}`);
+  });
+} else {
+  // ! usar este para el servidor remoto
+  const privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/e-control-pl.com/privkey.pem",
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/e-control-pl.com/cert.pem",
+    "utf8"
+  );
+  const ca = fs.readFileSync(
+    "/etc/letsencrypt/live/e-control-pl.com/chain.pem",
+    "utf8"
+  );
+  const credentials = { key: privateKey, cert: certificate, ca: ca };
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(PORT, IP, () => {
+    console.log(`Servidor en ejecución en https://${IP}:${PORT}`);
+  });
+}
